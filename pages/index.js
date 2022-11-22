6ac9f583c4b13118d5a36bcebb743f327e938c9c
2161351 <2161351@my.ipleiria.pt>
2022-11-22 16:51:18 +0000

    typescript fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export default function Home( { allPostsData }) {
+export default function Home( { allPostsData}) {
   return (
     <Layout home>
       <Head>
